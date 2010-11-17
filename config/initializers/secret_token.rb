c20d5a1bdbdabb3612a2e81b0b96314210acaaae
unknown <DavidAtLaptop@yahoo.com>
2010-11-17 09:24:36 -0800

    First commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jdrampage::Application.config.secret_token = '5771a47be9be6ef7d54333d90313ec06aa34baa86e20f09d05711f09caf854e78981cf7a1d477483a7eb161acc53b3acd62e8903a001f60d41e8a1f3994ff8bf'
