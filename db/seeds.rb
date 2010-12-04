7ba0c527847ff95f1352f557c56b8f814662d9b8
daze <divadz3@yahoo.com>
2010-12-04 16:22:26 -0500

    modified README and seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,12 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-User.create!(:login=>"dakota",:password=>"northandsouth",:password_confirmation=>"northandsouth",:email=>"dmonopoly10@gmail.com")
+User.create(:login=>"dakota",:password=>"northandsouth",:password_confirmation=>"northandsouth",:email=>"dmonopoly10@gmail.com")
+
+# Sections
+s = Section.create(:name => "Sports")
+n = Section.create(:name => "News")
+c = Section.create(:name => "Commentary")
+e = Section.create(:name => "Entertainment")
+f = Section.create(:name => "Features")
 
