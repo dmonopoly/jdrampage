b6695abf6c23e1ba1747c7d07584ac0a1c4123b1
David Zhang <dmonopoly10@gmail.com>
2010-12-10 09:43:29 -0800

    major changes to views and stylesheets; added missing.png

--- test/factories.rb
+++ test/factories.rb
@@ -4,18 +4,10 @@ Factory.define :article do |t|
 	t.body Forgery::LoremIpsum.paragraphs(5)
 	t.status "status here"
 	t.comments_allowed false
-	#t.section_id
-	#t.position
 	t.notes "notes here"
 	t.teaser "I am a teaser."
 	t.styles "styles here"
 	t.year "10-11"
-	#t.created_at
-	#t.updated_at
-	#t.photo_file_name
-	#t.photo_content_type
-	#t.photo_file_size
-	#t.photo_updated_at
 	t.association :section # 'belongs to a section'
 end
 
