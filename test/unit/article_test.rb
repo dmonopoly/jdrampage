acefbad894b68d5bb443d3068294c4778eba3c8c
David Zhang <dmonopoly10@gmail.com>
2010-12-20 19:38:26 -0800

    article_test.rb modified

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -16,7 +16,13 @@ class ArticleTest < ActiveSupport::TestCase
 		should validate_presence_of :body
 		should validate_presence_of :year
 		
-		#should_validate_inclusion_of :year, :in => %w[ 09-10 10-11 ]
+		should allow_value('09-10').for :year
+		should allow_value('10-11').for :year
+		should_not allow_value('2009-2010').for :year
+		should_not allow_value('2010-2011').for :year
+		should_not allow_value('08-09').for :year
+		should_not allow_value('11-12').for :year
+		#should ensure_inclusion_of(:year).in_range(%w[ 09-10 10-11 ])
 		
 		# the following need shoulda macros plugin installed first
 		#should_have_attached_file :photo
