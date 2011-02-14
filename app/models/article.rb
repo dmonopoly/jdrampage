6bf297e49ab89b5d416870d38be673eb9b69904f
David Zhang <dmonopoly10@gmail.com>
2011-02-13 20:04:00 -0500

    minor changes ImageMagick should work; just need to actually upload images, not use :default, to test

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  has_attached_file :photo, :default_url=>"/system/photos/original/default.jpg",#"/system/photos/original/missing.png",
+  has_attached_file :photo, :default_url=>"/system/photos/original/missing.png",#"/system/photos/original/default.jpg"
 										:styles => { 
 											:thumb => "100x100#",
 											:small => "150x150>",
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,2 @@
-#Paperclip.options[:image_magick_path] = "/home/daze/Documents/Install/ImageMagick-6.6.7-0"
-=begin
-def quote_command_options(*options) 
-	options.map do |option| 
-	option.split("'").map{|m| "#{m}" }.join("\\'") #removed single quotes around #{m} 
-end 
-=end
+#Paperclip.options[:command_path] = '/usr/local/bin'
+# delete this file?
