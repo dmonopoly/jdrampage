28f7a97055f75748a00980c1d7221e8a629f8e24
David Zhang <dmonopoly10@gmail.com>
2011-02-26 21:44:56 -0500

    stylesheet refactoring; decided to leave paperclip.rb commented (image uploading works on windows, not on linux); slight footer changes

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,7 @@
-#Paperclip.options[:command_path] = '/usr/local/bin'
-# delete this file?
+# Nothing is needed here is running on your Windows.
+
+# The following is for your Ubuntu 10.04.
+# Oddly, it does not work, although "which identify" yields "/usr/local/bin/identify"
+# Paperclip.options[:command_path] = "/usr/local/bin/"
+
+# For now, only use your Windows to deal with images b/c it doesn't work on your Ubuntu.
