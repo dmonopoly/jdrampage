2a146aa00abcd77387d47bb13dfa8fd92e4f2a01
David Zhang <dmonopoly10@gmail.com>
2011-02-26 23:01:05 -0500

    progress with uls in sections show - css done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
 	def logged_in? # self-created alias of current_user
 		current_user
 	end
+	
+	# used by backside
+	def sidebar?
+		params[:controller] == 'static' || (params[:controller] == 'admin/articles' && params[:action] == 'show')
+	end
 end
