c71c2b10dd046b3f251a2c7b0fe9d0ac7669a3d6
David Zhang <dmonopoly10@gmail.com>
2011-04-13 18:18:00 -0400

    refactored article partials; altered article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,9 +4,10 @@ class Article < ActiveRecord::Base
   belongs_to :section
 	has_attached_file :photo, :default_url=>"/system/photos/original/missing.png",#"/system/photos/original/default.jpg"
 										:styles => {
+											:large => "300x300",
+											:medium => "200x200>",
 											:thumb => "100x100#",
-											:small => "150x150>",
-											:medium => "200x200" }
+											 }
   acts_as_list
 
   validates_presence_of :section, :title, :author, :body, :date
