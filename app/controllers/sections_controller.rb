e2fddd5f5a3f94ec9d9ace4feba56c79ef90cba4
David Zhang <divadz3@yahoo.com>
2011-04-21 18:18:04 -0400

    improved image display drastically

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,8 +2,7 @@ class SectionsController < ApplicationController
 
   def show
     @section = Section.find(params[:id])
-		@articles = @section.articles.paginate :page => params[:page], :per_page => 10, :order => 'articles.section_position ASC'
+		@articles = @section.articles.paginate :page => params[:page], :per_page => 5, :order => 'articles.section_position ASC'
   end
 
 end
-
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,10 +4,11 @@ class Article < ActiveRecord::Base
   belongs_to :section
 	has_attached_file :photo, :default_url=>"/system/photos/original/missing.png",#"/system/photos/original/default.jpg"
 										:styles => {
-											:large => "300x300",
+											:display => "550x550>",
+											:large => "300x300>",
 											:medium => "200x200>",
 											:thumb => "100x100#",
-											 }
+										}
   acts_as_list
 
   validates_presence_of :section, :title, :author, :body, :date
