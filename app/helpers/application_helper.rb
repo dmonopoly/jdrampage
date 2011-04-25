359d9a45ab105bbd4389185b680aaea92593b439
David Zhang <dmonopoly10@gmail.com>
2011-04-24 20:33:05 -0400

    added teaser field to article form; identified problematic article columns

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,6 @@ module ApplicationHelper
 		current_user
 	end
 
-	# used by the back; probably should refactor w/content_for
-	def sidebar? # probably should change this - it doesn't follow rails conventions
-		params[:controller] == 'static' ||
-		(params[:controller] == 'admin/articles' && params[:action] == 'show') ||
-		(params[:controller] == 'admin/pages' && params[:action] == 'show')
-	end
-
 	# used by free spaces index
 	def yes_or_no(boolean)
 	  if boolean
