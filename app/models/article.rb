76f7ab1a2bc84b370fa1d0e66a76f6aa37ea74c8
David Zhang <dmonopoly10@gmail.com>
2011-05-17 09:06:29 -0400

    made author partial

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,4 @@
 class Article < ActiveRecord::Base
-  # include Paperclip ?
-  #has_many :comments, :dependent => :destroy
   belongs_to :section
 	has_attached_file :photo, :default_url=>"/system/photos/original/missing.png",#"/system/photos/original/default.jpg"
 										:styles => {
@@ -11,7 +9,7 @@ class Article < ActiveRecord::Base
 										}
   acts_as_list
 
-  validates_presence_of :section, :title, :author, :body, :date
-  validates_format_of :date, :with => /\d\d\d\d-\d\d-\d\d/
+  validates_presence_of :section, :title, :body, :date
 
 end
+
