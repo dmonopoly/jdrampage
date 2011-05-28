7f012d2831c3ffff8fb0c5c7567a27a747477ae6
David Zhang <divadz3@yahoo.com>
2011-05-28 18:20:57 -0400

    Edited config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,33 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
-
+set :application, "jdrampage"
+ 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
-
+set :deploy_to, "/var/www/jdrampage"
+ 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
+set :repository, "git@github.com:dmonopoly/jdrampage.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+set :use_sudo, false
+set :user, 'jdrampage'
+set :ssh_options, { :forward_agent => true }
+ 
+role :app, "97.107.132.47"
+role :web, "97.107.132.47"
+role :db,  "97.107.132.47", :primary => true
+ 
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
