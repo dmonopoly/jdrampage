e1082c92165ed73f41351f634d1eda657ed57969
David Zhang <dmonopoly10@gmail.com>
2011-06-02 17:51:29 -0400

    semicolon removed

--- db/migrate/20110602210413_change_column_type_for_free_space_content.rb
+++ db/migrate/20110602210413_change_column_type_for_free_space_content.rb
@@ -1,7 +1,8 @@
 class ChangeColumnTypeForFreeSpaceContent < ActiveRecord::Migration
   def self.up
     # from here on out, MySQL really should be the database instead of sqlite3
-    execute "ALTER TABLE 'free_spaces' ALTER_COLUMN 'content' DROP DEFAULT;"
+    # MySQL version: Ver 14.12 Distrib 5.0.83
+    execute "ALTER TABLE 'free_spaces' ALTER_COLUMN 'content' DROP DEFAULT"
 
     change_column :free_spaces, :content, :text, :limit => nil
   end
