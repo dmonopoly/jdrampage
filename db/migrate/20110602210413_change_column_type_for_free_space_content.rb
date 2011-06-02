5dcc1e4ef7adfbc415cda1ba1d63cd3dfcb1d2a6
David Zhang <dmonopoly10@gmail.com>
2011-06-02 17:52:54 -0400

    more silly changes

--- db/migrate/20110602210413_change_column_type_for_free_space_content.rb
+++ db/migrate/20110602210413_change_column_type_for_free_space_content.rb
@@ -2,7 +2,7 @@ class ChangeColumnTypeForFreeSpaceContent < ActiveRecord::Migration
   def self.up
     # from here on out, MySQL really should be the database instead of sqlite3
     # MySQL version: Ver 14.12 Distrib 5.0.83
-    execute "ALTER TABLE 'free_spaces' ALTER_COLUMN 'content' DROP DEFAULT"
+    execute "ALTER TABLE 'free_spaces' ALTER COLUMN 'content' DROP DEFAULT"
 
     change_column :free_spaces, :content, :text, :limit => nil
   end
