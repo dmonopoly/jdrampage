948739e3f9b2c27c6fc4a8eb2756bb84be8b118d
David Zhang <dzhang21@gmail.com>
2011-06-22 21:12:15 -0400

    brought back all pages in section show

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,8 +2,7 @@ class SectionsController < ApplicationController
 
   def show
     @section = Section.find(params[:id])
-		@articles = @section.articles.paginate :page => params[:page], :per_page => 6, :order => 'articles.section_position ASC',
-		                                        :total_entries => 10 # TEMPORARY to limit number of pages b/c of utf8 problem
+		@articles = @section.articles.paginate :page => params[:page], :per_page => 6, :order => 'articles.section_position ASC'#, :total_entries => 10 # TEMPORARY to limit number of pages b/c of utf8 problem
   end
 
 end
